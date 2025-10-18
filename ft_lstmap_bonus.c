c00a31892c335c95110fea2dc24180238fcbeec3
Minipl0p <paul.chazalmartin@protonmail.com>
2025-10-19 00:26:18 +0200

    version juste

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,27 +6,29 @@
 /*   By: pchazalm <pchazalm@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 22:42:31 by pchazalm          #+#    #+#             */
-/*   Updated: 2025/10/17 23:27:20 by pchazalm         ###   ########.fr       */
+/*   Updated: 2025/10/19 00:12:22 by pchazalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
+#include <stdlib.h>
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*n_list;
 	t_list	*n_elem;
+	void	*n_content;
 
-	if (!lst || !f || !del)
-		return (NULL);
-	n_list = ft_lstnew(f(lst->content));
-	lst = lst->next;
+	n_list = NULL;
 	while (lst)
 	{
-		n_elem = ft_lstnew(f(lst->content));
+		n_content = (*f)(lst->content);
+		n_elem = ft_lstnew(n_content);
 		if (!n_elem)
 		{
+			del(n_content);
+			free(n_elem);
 			ft_lstclear(&n_list, del);
 			return (NULL);
 		}
