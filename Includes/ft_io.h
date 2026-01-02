f0d4c44ffc998ef7769ec0aa0788bbd2a45003d4
Paul <chazalmpartin.paul@gmail.com>
2026-01-02 10:14:09 +0100

    fix heder io.h

--- Includes/ft_io.h
+++ Includes/ft_io.h
@@ -6,7 +6,7 @@
 /*   By: pchazalm <pchazalm@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 15:45:27 by pchazalm          #+#    #+#             */
-/*   Updated: 2025/12/30 16:18:12 by pchazalm         ###   ########.fr       */
+/*   Updated: 2026/01/02 10:13:43 by miniplop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-void	ft_puterror(char *s);
+void	ft_puterror(void);
 
 #endif
