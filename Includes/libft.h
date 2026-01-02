220c8a09f579fb935ebcea726310e01e1d41df10
Paul <chazalmpartin.paul@gmail.com>
2026-01-02 12:39:35 +0100

    fix header libft.h

--- Includes/libft.h
+++ Includes/libft.h
@@ -6,23 +6,23 @@
 /*   By: pchazalm <pchazalm@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 18:53:28 by pchazalm          #+#    #+#             */
-/*   Updated: 2025/12/31 16:17:07 by miniplop         ###   ########.fr       */
+/*   Updated: 2026/01/02 12:39:19 by miniplop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <unistd.h>
-# include "ft_char.h"
-# include "ft_string.h"
-# include "ft_memory.h"
-# include "ft_io.h"
-# include "ft_printf.h"
-# include "ft_list.h"
-# include "ft_utils.h"
-# include "ft_gnl.h"
 # include "ft_btree.h"
-# include "ft_listdb.h"
+# include "ft_char.h"
+# include "ft_convert.h"
 # include "ft_dict.h"
+# include "ft_gnl.h"
+# include "ft_io.h"
+# include "ft_list.h"
+# include "ft_listdb.h"
+# include "ft_memory.h"
+# include "ft_printf.h"
+# include "ft_string.h"
+# include "ft_utils.h"
 
 #endif
